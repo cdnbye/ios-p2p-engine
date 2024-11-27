467bceaa6fc2501bbc25ca9f01ab90bfcb67f232
snowinszu <86755838@qq.com>
2024-11-27 17:36:28 +0800

    Initial Commit

---
+++ swiftui-demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-demo
+//
+//  Created by timmy on 2024/11/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-demo/swiftui_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_demoApp.swift
+//  swiftui-demo
+//
+//  Created by timmy on 2024/11/27.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
